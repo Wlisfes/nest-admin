50b60461ba2270a08f6130aae640685e2b86fbcc
fanyanghua <876451336@qq.com>
2022-04-22 00:18:08 +0800

    🐞 fix(菜单模块): 菜单滚动修复

--- src/router/auth-routes.ts
+++ src/router/auth-routes.ts
@@ -102,6 +102,134 @@ export const authRoutes: RouteRecordRaw[] = [
 		]
 	},
 	{
+		path: '/http',
+		meta: { title: '异常页面', icon: 'antd-home' },
+		component: Layout,
+		redirect: '/http/401',
+		children: [
+			{
+				path: '/http/401',
+				name: 'http401',
+				meta: { title: '401' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/403',
+				name: 'http403',
+				meta: { title: '403' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/404',
+				name: 'http404',
+				meta: { title: '404' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/405',
+				name: 'http405',
+				meta: { title: '405' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/406',
+				name: 'http406',
+				meta: { title: '406' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/407',
+				name: 'http407',
+				meta: { title: '407' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/408',
+				name: 'http408',
+				meta: { title: '408' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/409',
+				name: 'http409',
+				meta: { title: '409' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/410',
+				name: 'http410',
+				meta: { title: '410' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/411',
+				name: 'http411',
+				meta: { title: '411' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/412',
+				name: 'http412',
+				meta: { title: '412' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/413',
+				name: 'http413',
+				meta: { title: '413' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/414',
+				name: 'http414',
+				meta: { title: '414' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/415',
+				name: 'http415',
+				meta: { title: '415' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/416',
+				name: 'http416',
+				meta: { title: '416' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/417',
+				name: 'http417',
+				meta: { title: '417' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/417',
+				name: 'http417',
+				meta: { title: '417' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/418',
+				name: 'http418',
+				meta: { title: '418' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/419',
+				name: 'http419',
+				meta: { title: '419' },
+				component: () => import('@/views/home/Template.vue')
+			},
+			{
+				path: '/http/420',
+				name: 'http420',
+				meta: { title: '420' },
+				component: () => import('@/views/home/Template.vue')
+			}
+		]
+	},
+	{
 		path: '/about',
 		meta: { root: true },
 		component: Layout,
