d60da6298f486f250a355ad92fcf9981b51775d3
fanyanghua <876451336@qq.com>
2022-05-17 23:36:54 +0800

    ✨ feat(Aliyun): 新增视频点播封装方法

--- src/naive-ui.d.ts
+++ src/naive-ui.d.ts
@@ -3,5 +3,8 @@ import type { LoadingBarInst } from 'naive-ui/lib/loading-bar/index'
 declare global {
 	interface Window {
 		$loading: LoadingBarInst
+		AliyunUpload: {
+			Vod: new (option) => any
+		}
 	}
 }
