36bad806f1c05887afe8d8dfb8c66f64587c996c
fanyanghua <876451336@qq.com>
2022-05-18 23:27:46 +0800

    ✨ feat(player): 新增播放器实例

---
+++ src/utils/utils-player.ts
@@ -0,0 +1,23 @@
+import DPlayer from 'dplayer'
+
+export interface DoneOption {
+	el: HTMLElement | null
+	url: string
+	cover?: string
+	autoplay?: boolean
+	theme?: string
+	lang?: 'en' | 'zh-cn' | 'zh-tw'
+}
+export function initPlayer(option: DoneOption) {
+	const client = new DPlayer({
+		container: option.el,
+		theme: option.theme ?? '#fb7299',
+		lang: option.lang ?? 'zh-cn',
+		video: {
+			url: option.url,
+			pic: option.cover
+		}
+	})
+
+	return { client }
+}
