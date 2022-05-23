07edd0a95b05b3ef61e7b9ce071dbfc2f577a15a
lifei <lifei1342@dingtalk.com>
2022-05-23 17:43:19 +0800

    🎈 perf(api): 接口添加类型提示

--- src/api/service/aliyun-service.ts
+++ src/api/service/aliyun-service.ts
@@ -1,6 +1,7 @@
 import { request } from '@/utils/utils-request'
+import type { IDemanOption } from '@/api/pipe'
 
 /**获取播放信息**/
 export function httpCloudCheck(params: { VideoId: string; AuthTimeout: number }) {
-	return request({ url: `/api/aliyun/play-info`, method: 'GET', params })
+	return request<IDemanOption>({ url: `/api/aliyun/play-info`, method: 'GET', params })
 }
--- src/api/service/cloud-service.ts
+++ src/api/service/cloud-service.ts
@@ -1,11 +1,12 @@
 import { request } from '@/utils/utils-request'
+import type { ICloudOption } from '@/api/pipe'
 
 /**音视频列表-客户端**/
 export function httpClientClouds(params: { page: number; size: number; type?: number; title?: string }) {
-	return request({ url: `/api/cloud/client/list-node`, method: 'GET', params })
+	return request<Array<ICloudOption>>({ url: `/api/cloud/client/list-node`, method: 'GET', params })
 }
 
 /**音视频详情-客户端**/
 export function httpClientCloud(params: { id: number }) {
-	return request({ url: `/api/cloud/client/info`, method: 'GET', params })
+	return request<ICloudOption>({ url: `/api/cloud/client/info`, method: 'GET', params })
 }
