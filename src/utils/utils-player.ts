f2c9d29cdf5e76ac16da9d646b7efdf3c0736f69
lifei <lifei1342@dingtalk.com>
2022-05-24 14:04:17 +0800

    ✨ feat(player): 播放器添加flv文件支持

--- src/utils/utils-player.ts
+++ src/utils/utils-player.ts
@@ -1,4 +1,5 @@
-import DPlayer from 'dplayer'
+import DPlayer, { DPlayerVideo } from 'dplayer'
+import flv from 'flv.js'
 
 export interface DoneOption {
 	el: HTMLElement | null
@@ -10,16 +11,28 @@ export interface DoneOption {
 }
 
 export function initPlayer(option: DoneOption) {
-	const client = new DPlayer({
+	const format = option.url.split('.').pop()?.split('?')?.shift()?.toLocaleUpperCase()
+	let video: DPlayerVideo = { type: 'auto', url: option.url, pic: option.cover }
+	switch (format) {
+		case 'FLV':
+			video = Object.assign(video, {
+				type: 'customFlv',
+				customType: {
+					customFlv: (video: HTMLVideoElement) => {
+						const player = flv.createPlayer({ type: 'flv', url: video.src })
+						player.attachMediaElement(video)
+						player.load()
+					}
+				}
+			})
+			break
+	}
+
+	return new DPlayer({
 		container: option.el,
 		autoplay: option.autoplay ?? false,
 		theme: option.theme ?? '#fb7299',
 		lang: option.lang ?? 'zh-cn',
-		video: {
-			url: option.url,
-			pic: option.cover
-		}
+		video
 	})
-
-	return client
 }
