7ead880634212be621283f760b936c33872609ba
妖雨纯 <876451336@qq.com>
2022-05-28 22:18:36 +0800

    ✨ feat(client): 添加客户端界面

---
+++ src/client/Multiple.vue
@@ -0,0 +1,21 @@
+<script lang="tsx">
+import { defineComponent, ref, computed } from 'vue'
+
+export default defineComponent({
+	name: 'Multiple',
+	setup() {
+		return () => <div></div>
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+.app-base {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	z-index: 10;
+	position: relative;
+	overflow: hidden;
+}
+</style>
