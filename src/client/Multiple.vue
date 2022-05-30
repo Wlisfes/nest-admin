8f9dc879878c1800a5e8f7a0f5e5abbd6ea66859
妖雨纯 <876451336@qq.com>
2022-05-30 17:32:10 +0800

    🌈 style(vue-i18n): 添加vue-i18n使用方式

--- src/client/Multiple.vue
+++ src/client/Multiple.vue
@@ -1,16 +1,19 @@
 <script lang="tsx">
 import { defineComponent, ref, computed } from 'vue'
 import { instance } from '@/utils/utils-watcher'
+import { useLocale } from '@/lang'
 
 export default defineComponent({
 	name: 'Multiple',
 	setup() {
+		const { t, onChange, locale } = useLocale()
 		instance.observer.on('scroll', response => {
 			console.log(response)
 		})
 
 		return () => (
 			<div>
+				<n-button onClick={() => onChange(locale.value === 'en' ? 'zh' : 'en')}>{t('name')}</n-button>
 				{Object.keys([...Array(20)]).map(k => (
 					<h1 key={k}>{k}</h1>
 				))}
