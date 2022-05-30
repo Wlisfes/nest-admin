6dc39d1c549ecae2daa824c4a3dbd070149daf2c
妖雨纯 <876451336@qq.com>
2022-05-30 17:28:22 +0800

    ✨ feat(empty): 新增空状态组件

--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,3 +1,4 @@
 export { default as AppProvider } from './app-provider.vue'
 export { default as AppContainer } from './app-container.vue'
+export { default as UEmpty } from './u-empty.vue'
 export { default as UIcon } from './u-icon.vue'
