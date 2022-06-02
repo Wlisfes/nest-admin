fcce06b50874e71a2bd53075233fb1dffe4b5274
妖雨纯 <876451336@qq.com>
2022-06-02 22:33:05 +0800

    🌈 style(Multiple): 关闭单例模式滚动

--- src/client/Multiple.vue
+++ src/client/Multiple.vue
@@ -1,35 +1,37 @@
 <script lang="tsx">
-import { defineComponent, ref, computed } from 'vue'
-import { instance } from '@/utils/utils-watcher'
-import { useLocale } from '@/lang'
+import type { IPartner, IPoster } from '@/api/pipe'
+import { defineComponent, ref, nextTick, onUnmounted } from 'vue'
+import { AppContainer } from '@/components/global'
+import { httpClientPartner } from '@/api/service'
+import { initMounte } from '@/utils/utils-tool'
 
 export default defineComponent({
 	name: 'Multiple',
 	setup() {
-		const { t, onChange, locale } = useLocale()
-		instance.observer.on('scroll', response => {
-			console.log(response)
-		})
+		const page = ref<number>(1)
+		const size = ref<number>(10)
+		const total = ref<number>(0)
+		const more = ref<boolean>(true)
+		const loading = ref<boolean>(true)
+		const dataSource = ref<Array<IPartner>>([])
 
 		return () => (
-			<div>
-				<n-button onClick={() => onChange(locale.value === 'en' ? 'zh' : 'en')}>{t('name')}</n-button>
-				{Object.keys([...Array(20)]).map(k => (
-					<h1 key={k}>{k}</h1>
-				))}
-			</div>
+			<AppContainer
+				class="app-pipe"
+				loading={loading.value && total.value === 0}
+				empty={!loading.value && total.value === 0}
+			></AppContainer>
 		)
 	}
 })
 </script>
 
 <style lang="scss" scoped>
-.app-base {
-	flex: 1;
-	display: flex;
-	flex-direction: column;
-	z-index: 10;
-	position: relative;
-	overflow: hidden;
+.app-pipe {
+	width: 100%;
+	max-width: 1280px;
+	padding: 0 15px;
+	margin: 80px auto;
+	box-sizing: border-box;
 }
 </style>
