0a3a8e462cfd113d76b1d42efc59d7eada3acce4
妖雨纯 <876451336@qq.com>
2022-06-08 17:19:12 +0800

    ✨ feat(editor): 修改编辑器库

--- src/hooks/hook-editor.tsx
+++ src/hooks/hook-editor.tsx
@@ -1,11 +1,43 @@
-export { default as MdEditor } from 'md-editor-v3'
+import type { App } from 'vue'
+import MavonEditor from 'mavon-editor'
 import { ref, onMounted, defineComponent, computed, CSSProperties } from 'vue'
 import { useDvcStore } from '@/store/modules/dvc-store'
 
-export function useEditor() {
-	const dvc = useDvcStore()
-	const preview = ref('app-editor-preview')
-	const theme = computed(() => dvc.theme)
+export const NPreview = defineComponent({
+	name: 'NPreview',
+	props: { value: String },
+	setup(props) {
+		const preview = ref<HTMLElement>()
+		const dvc = useDvcStore()
 
-	return { theme, preview }
+		/**删除无用help容器**/
+		const onRemove = () => {
+			const el = preview.value as unknown as { $refs: { help: HTMLElement } }
+			el.$refs.help.remove()
+		}
+
+		onMounted(() => onRemove())
+
+		return () => (
+			<mavon-editor
+				class="app-preview"
+				ref={preview}
+				subfield={false}
+				toolbarsFlag={false}
+				autofocus={false}
+				shortCut={false}
+				boxShadow={false}
+				editable={false}
+				transition={false}
+				tabSize={4}
+				defaultOpen="preview"
+				codeStyle="atom-one-dark"
+				model-value={props.value}
+			></mavon-editor>
+		)
+	}
+})
+
+export function setupEditor(app: App<Element>) {
+	app.use(MavonEditor)
 }
