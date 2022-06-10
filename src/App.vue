28e694638e353cf710913ef5cf9b5db0f6d575ca
妖雨纯 <876451336@qq.com>
2022-06-10 10:36:51 +0800

    🦄 refactor(instance): 修改单例模式工具为hook方式引用

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,28 @@ import { defineComponent, onMounted, onUnmounted } from 'vue'
 import { RouterView } from 'vue-router'
 import { useAppStore } from '@/store/modules/app-store'
 import { routes } from '@/router'
-import { client } from '@/utils/utils-instance'
+import { useClient } from '@/utils/utils-instance'
 import { useToRoute } from '@/utils/utils-route'
 import { AppProvider } from '@/components/global'
 
 export default defineComponent({
-	name: 'App',
-	setup() {
-		const store = useAppStore()
-		store.setRouter(useToRoute(routes))
-		store.initBanner()
+    name: 'App',
+    setup() {
+        const { client } = useClient()
+        const store = useAppStore()
+        store.setRouter(useToRoute(routes))
+        store.initBanner()
 
-		onUnmounted(() => client.off())
-		onMounted(() => {
-			client.listener().then(() => client.on())
-		})
+        onUnmounted(() => client.off())
+        onMounted(() => {
+            client.listener().then(() => client.on())
+        })
 
-		return () => (
-			<AppProvider>
-				<RouterView></RouterView>
-			</AppProvider>
-		)
-	}
+        return () => (
+            <AppProvider>
+                <RouterView></RouterView>
+            </AppProvider>
+        )
+    }
 })
 </script>
--- src/utils/utils-instance.ts
+++ src/utils/utils-instance.ts
@@ -1,55 +1,68 @@
+import { ref, onMounted } from 'vue'
 import { Observer } from '@/utils/utils-observer'
 import { useAppStore } from '@/store/modules/app-store'
 
 type ClientEvent = {
-	resize: { width: number; device: string; collapse: boolean }
+    resize: { width: number; device: string; collapse: boolean }
 }
 
 export const client = new (class Client {
-	public observer = new Observer<ClientEvent>()
-	public store: any = null
+    public observer = new Observer<ClientEvent>()
+    public store: any = null
 
-	public async listener() {
-		const width = document.body.getBoundingClientRect().width
-		if (!this.store) {
-			this.store = useAppStore()
-		}
+    public async listener() {
+        const width = document.body.getBoundingClientRect().width
+        if (!this.store) {
+            this.store = useAppStore()
+        }
 
-		if (width > 1080) {
-			this.store.setDevice('PC')
-			this.store.setCollapse(false)
-			this.observer.emit('resize', { width, device: 'PC', collapse: false })
-		} else if (width > 768) {
-			this.store.setDevice('IPAD')
-			this.store.setCollapse(true)
-			this.observer.emit('resize', { width, device: 'IPAD', collapse: true })
-		} else {
-			this.store.setDevice('MOBILE')
-			this.store.setCollapse(true)
-			this.observer.emit('resize', { width, device: 'MOBILE', collapse: false })
-		}
-		return this.store.setWidth(width)
-	}
+        if (width > 1080) {
+            this.store.setDevice('PC')
+            this.store.setCollapse(false)
+            this.observer.emit('resize', { width, device: 'PC', collapse: false })
+        } else if (width > 768) {
+            this.store.setDevice('IPAD')
+            this.store.setCollapse(true)
+            this.observer.emit('resize', { width, device: 'IPAD', collapse: true })
+        } else {
+            this.store.setDevice('MOBILE')
+            this.store.setCollapse(true)
+            this.observer.emit('resize', { width, device: 'MOBILE', collapse: false })
+        }
+        return this.store.setWidth(width)
+    }
 
-	public async on() {
-		return window.addEventListener('resize', this.listener.bind(this))
-	}
+    public async on() {
+        return window.addEventListener('resize', this.listener.bind(this))
+    }
 
-	public async off() {
-		return window.removeEventListener('resize', this.listener.bind(this))
-	}
+    public async off() {
+        return window.removeEventListener('resize', this.listener.bind(this))
+    }
 })()
+export function useClient() {
+    return { client }
+}
 
 /***************************************************************************************/
 type IScroll = { distance: number; spin: boolean }
 type InstanceEvent = {
-	scroll: IScroll
+    scroll: IScroll
 }
 export const instance = new (class {
-	public observer = new Observer<InstanceEvent>()
+    public observer = new Observer<InstanceEvent>()
 
-	/**滚动事件**/
-	public onScrollbar(node: IScroll) {
-		this.observer.emit('scroll', node)
-	}
+    /**滚动事件**/
+    public onScrollbar(node: IScroll) {
+        this.observer.emit('scroll', node)
+    }
 })()
+export function useBatter() {
+    const current = ref<boolean>(true)
+
+    const setCurrent = (value: boolean) => {
+        current.value = value
+    }
+
+    return { current, setCurrent, instance }
+}
