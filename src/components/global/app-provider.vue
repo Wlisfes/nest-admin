b357570b021b2a26dd675e98530819887acdee78
妖雨纯 <876451336@qq.com>
2022-06-10 22:20:39 +0800

    🎉 init(naive): 新增全局反馈组件注入

--- src/components/global/app-provider.vue
+++ src/components/global/app-provider.vue
@@ -1,36 +1,38 @@
 <script lang="tsx">
 import { defineComponent, Fragment } from 'vue'
-import { useLoadingBar, useNotification } from 'naive-ui'
+import { useLoadingBar, useNotification, useMessage, useDialog } from 'naive-ui'
 import { useProvider } from '@/hooks/hook-provider'
 
 const RComponent = defineComponent({
-	name: 'RComponent',
-	setup(props, { slots }) {
-		window.$loading = useLoadingBar()
-		window.$notification = useNotification()
+    name: 'RComponent',
+    setup(props, { slots }) {
+        window.$loading = useLoadingBar()
+        window.$notification = useNotification()
+        window.$message = useMessage()
+        window.$dialog = useDialog()
 
-		return () => <Fragment>{slots.default?.()}</Fragment>
-	}
+        return () => <Fragment>{slots.default?.()}</Fragment>
+    }
 })
 
 export default defineComponent({
-	name: 'AppProvider',
-	setup(props, { slots }) {
-		const { theme, themeOverrides } = useProvider()
+    name: 'AppProvider',
+    setup(props, { slots }) {
+        const { theme, themeOverrides } = useProvider()
 
-		return () => (
-			<n-config-provider abstract theme={theme.value} theme-overrides={themeOverrides.value}>
-				<n-loading-bar-provider>
-					<n-dialog-provider>
-						<n-notification-provider max={3}>
-							<n-message-provider>
-								<RComponent>{{ default: slots.default }}</RComponent>
-							</n-message-provider>
-						</n-notification-provider>
-					</n-dialog-provider>
-				</n-loading-bar-provider>
-			</n-config-provider>
-		)
-	}
+        return () => (
+            <n-config-provider abstract theme={theme.value} theme-overrides={themeOverrides.value}>
+                <n-loading-bar-provider>
+                    <n-dialog-provider>
+                        <n-notification-provider max={3}>
+                            <n-message-provider>
+                                <RComponent>{{ default: slots.default }}</RComponent>
+                            </n-message-provider>
+                        </n-notification-provider>
+                    </n-dialog-provider>
+                </n-loading-bar-provider>
+            </n-config-provider>
+        )
+    }
 })
 </script>
--- src/naive-ui.d.ts
+++ src/naive-ui.d.ts
@@ -1,12 +1,16 @@
-import type { LoadingBarInst } from 'naive-ui/lib/loading-bar/index'
-import type { NotificationApiInjection } from 'naive-ui/lib/notification/index'
+import type { LoadingBarInst } from 'naive-ui/lib/loading-bar'
+import type { NotificationApiInjection } from 'naive-ui/lib/notification'
+import type { DialogApiInjection } from 'naive-ui/lib/dialog'
+import type { MessageApiInjection } from 'naive-ui/lib/message'
 
 declare global {
-	interface Window {
-		$loading: LoadingBarInst
-		$notification: NotificationApiInjection
-		AliyunUpload: {
-			Vod: new (option) => any
-		}
-	}
+    interface Window {
+        $message: MessageApiInjection
+        $loading: LoadingBarInst
+        $notification: NotificationApiInjection
+        $dialog: DialogApiInjection
+        AliyunUpload: {
+            Vod: new (option) => any
+        }
+    }
 }
