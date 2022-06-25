1ed145f20352b9513b78fa7e48746550ea08be08
妖雨纯 <876451336@qq.com>
2022-06-25 20:50:11 +0800

    ✨ feat(配置): 修改基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,45 +7,46 @@ import Vue from '@vitejs/plugin-vue'
 import VueJSX from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
-	const root = process.cwd()
-	const env = loadEnv(mode, root)
+    const root = process.cwd()
+    const env = loadEnv(mode, root)
 
-	return {
-		plugins: [
-			Vue(),
-			VueJSX(),
-			AutoImport({
-				resolvers: [NaiveUiResolver()]
-			}),
-			Components({
-				dts: true,
-				dirs: ['src/components'],
-				resolvers: [NaiveUiResolver()]
-			})
-		],
-		resolve: {
-			alias: {
-				'@': resolve(process.cwd(), 'src'),
-				'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
-			}
-		},
-		css: {
-			modules: {
-				generateScopedName: '[local]___[hash:5]',
-				localsConvention: 'camelCase'
-			}
-		},
-		server: {
-			port: Number(env.VITE_PORT),
-			open: true,
-			proxy: {
-				[env.VITE_API_BASE]: {
-					target: env.VITE_API_TARGET,
-					ws: true,
-					changeOrigin: true,
-					rewrite: path => path.replace(new RegExp(`^${env.VITE_API_BASE}`), '')
-				}
-			}
-		}
-	}
+    return {
+        plugins: [
+            Vue(),
+            VueJSX(),
+            AutoImport({
+                resolvers: [NaiveUiResolver()]
+            }),
+            Components({
+                dts: true,
+                dirs: ['src/components'],
+                resolvers: [NaiveUiResolver()]
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': resolve(process.cwd(), 'src'),
+                'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+            }
+        },
+        css: {
+            modules: {
+                generateScopedName: '[local]___[hash:5]',
+                localsConvention: 'camelCase'
+            }
+        },
+        server: {
+            port: Number(env.VITE_PORT),
+            open: true,
+            host: '0.0.0.0',
+            proxy: {
+                [env.VITE_API_BASE]: {
+                    target: env.VITE_API_TARGET,
+                    ws: true,
+                    changeOrigin: true,
+                    rewrite: path => path.replace(new RegExp(`^${env.VITE_API_BASE}`), '')
+                }
+            }
+        }
+    }
 })
