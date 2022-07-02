799890b3e527ef96a25b235e34ee3054fae117de
妖雨纯 <876451336@qq.com>
2022-07-02 11:28:12 +0800

    ✨ feat(App): 修改App实例配置

--- src/utils/utils-app.ts
+++ src/utils/utils-app.ts
@@ -1,21 +1,56 @@
-import { createApp, createVNode, type Component } from 'vue'
+import { createApp, createVNode, nextTick, type Component } from 'vue'
 import { setupStore } from '@/store'
 import { setupRouter } from '@/router'
 import { setupI18n } from '@/lang'
 import { setupEditor } from '@/hooks/hook-editor'
 import { AppProvider } from '@/components/global'
 
-export function createComponent(rootComponent: Component) {
+type IParameter = {}
+
+export function createComponent(rootComponent: Component, parameter?: IParameter) {
+    const el = document.createElement('div')
     const app = createApp(
         createVNode(AppProvider, null, {
             default: () => createVNode(rootComponent)
         })
     )
 
+    const mounte = (to?: HTMLElement, handler?: Function): Promise<void> => {
+        return new Promise((resolve, reject) => {
+            try {
+                app.mount(el, true)
+                if (to) {
+                    to.appendChild(el)
+                } else {
+                    document.body.appendChild(el)
+                }
+                handler?.()
+                resolve()
+            } catch (e: unknown) {
+                reject(e)
+            }
+        })
+    }
+
+    const unmount = (handler?: Function): Promise<void> => {
+        return new Promise((resolve, reject) => {
+            try {
+                app.unmount()
+                el.remove()
+                nextTick(() => {
+                    handler?.()
+                    resolve()
+                })
+            } catch (e: unknown) {
+                reject(e)
+            }
+        })
+    }
+
     setupI18n(app) //国际化
     setupStore(app) //挂载store
     setupRouter(app) //挂载路由
     setupEditor(app) //注册富文本组件
 
-    return { app }
+    return { app, el, mounte, unmount }
 }
